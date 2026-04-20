412da41c4be6689272e4343a6d8e8707da424486
Lucas Tepedino <ligeiro.dig@gmail.com>
2026-04-20 11:42:21 -0300

    aluguel de jogos

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function alterarStatus(id) {
+    let jogoSelecionado = document.getElementById(`game-${id}`);
+    let imagem = jogoSelecionado.querySelector('.dashboard__item__img');
+    let botao = jogoSelecionado.querySelector('.dashboard__item__button');
+    
+
+    if (botao.classList.contains('dashboard__item__button--return')) {
+        botao.classList.remove('dashboard__item__button--return');
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.innerHTML = 'Alugar';
+    } else {
+        botao.classList.add('dashboard__item__button--return')
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.innerHTML = 'Devolver';
+    }
+}
+
