40c182b50622bc267f0458ac0fef81ddd2b6e63f
Lucas Tepedino <ligeiro.dig@gmail.com>
2026-05-02 10:41:17 -0300

    codigo restaurado

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
+function alterarStatus(id) {
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let botao = jogoClicado.querySelector('.dashboard__item__button');
 
+    if (botao.classList.contains('button dashboard__item__button--return')) {
+        botao.classList.remove('button dashboard__item__button--return');
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.textContent = 'Alugar';
+    } else {
+        botao.classList.add('button dashboard__item__button--return');
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+    }
+}
 
